8acdb79be2b4cc34aa35a20402145b30b2fb66c6
Felipe Cepluki <felipecepluki@gmail.com>
2023-05-19 12:08:40 -0300

    feat(class 3): 50% of the class

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -14,8 +14,17 @@ import { styled } from 'nativewind'
 import blurBg from './src/assets/bg-blur.png'
 import Stripes from './src/assets/stripes.svg'
 import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
+import { useEffect } from 'react'
 
 const StyledStripes = styled(Stripes)
+
+const discovery = {
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
+  revocationEndpoint:
+    'https://github.com/settings/connections/applications/64e1c30ace3d30e0adf4',
+}
 
 export default function App() {
   const [hasLoadedFonts] = useFonts({
@@ -23,6 +32,24 @@ export default function App() {
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
+
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      clientId: '64e1c30ace3d30e0adf4',
+      scopes: ['identity'],
+      redirectUri: makeRedirectUri({
+        scheme: 'nlwspacetime',
+      }),
+    },
+    discovery,
+  )
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params
+      console.log(code)
+    }
+  }, [response])
 
   if (!hasLoadedFonts) {
     return null
@@ -49,6 +76,7 @@ export default function App() {
         <TouchableOpacity
           activeOpacity={0.7}
           className="rounded-full bg-green-500 px-5 py-3"
+          onPress={() => signInWithGithub()}
         >
           <Text className="font-alt text-sm uppercase text-black">
             Cadastrar lembrança
